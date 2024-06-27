b4ec7f82a5853ef31e2168d1c65d16b8dbcacbe2
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-27 16:03:49 +0200

    [UPD]Added 1 to 100 count in script.js and in console log

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 //Ciclo i numeri da 1 a 100
 
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    
 //Se il valore del numero che sto attualmente ciclando è un multiplo di tre, mostro il testo Fizz
 
 //Se il valore del numero che sto attualmente ciclando è un numero multiplo di cinque, mostro il testo Buzz
 
 //Altrimenti se è sia un multiplo di 3 che di 5, mostro il testo FizzBuzz
+
+}
+
