e30d5549be0db3fc9dc81062785262c0b65e201c
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-27 16:40:24 +0200

    [FIX]Now the wording is correct: Fizz, Buzz and FizzBuzz

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
     //Se è sia un multiplo di 3 che di 5, mostro il testo FizzBuzz
     if (i % 3 === 0 && i % 5 ===0){
-        console.log(`${i} multiplo di 3 e 5`)
+        console.log(`${i} FizzBuzz`)
     }
     //Se il valore del numero che sto attualmente ciclando è un multiplo di tre, mostro il testo Fizz
     else if (i % 3 == 0){
-        console.log(`${i} multiplo di 3`)
+        console.log(`${i} Fizz`)
     }
     //Se il valore del numero che sto attualmente ciclando è un numero multiplo di cinque, mostro il testo Buzz
     else if (i % 5 == 0){
-        console.log(`${i} multiplo di 5`)
+        console.log(`${i} Buzz`)
     }
 }
 
