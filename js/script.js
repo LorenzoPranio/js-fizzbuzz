63f73872620fca0476afcf2c6068cd5527e1df09
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-27 15:44:16 +0200

    [UPD]Added the text that acts as a guide for carrying out the exercise in script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Ciclo i numeri da 1 a 100
+
+//Se il valore del numero che sto attualmente ciclando è un multiplo di tre, mostro il testo Fizz
+
+//Se il valore del numero che sto attualmente ciclando è un numero multiplo di cinque, mostro il testo Buzz
+
+//Altrimenti se è sia un multiplo di 3 che di 5, mostro il testo FizzBuzz
