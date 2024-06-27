620bf575d857a8165759eb0e79f69caa8ac53aee
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-27 16:14:03 +0200

    [UPD]Added in console log the wording: multiple of 5

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 == 0){
         console.log(`${i} multiplo di 3`)
     }
+    //Se il valore del numero che sto attualmente ciclando è un numero multiplo di cinque, mostro il testo Buzz
+    else if (i % 5 == 0){
+        console.log(`${i} multiplo di 5`)
+    }
 
 
-//Se il valore del numero che sto attualmente ciclando è un numero multiplo di cinque, mostro il testo Buzz
 
 //Altrimenti se è sia un multiplo di 3 che di 5, mostro il testo FizzBuzz
 
